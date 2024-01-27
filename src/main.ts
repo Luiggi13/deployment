b9b15c7c02cd63b0631bfcadacc58dab75065dbb
Christian Llansola <christian.llansola@mail.schwarz>
2024-01-27 13:18:20 +0100

    Cors updated

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+    credentials: true,
+    methods: ['*'],
+    allowedHeaders: ['*'],
+  });
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
