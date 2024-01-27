96c6d0d2defb6bfba0a6d2d1314d831b0ba67d36
Christian Llansola <christian.llansola@mail.schwarz>
2024-01-27 15:40:24 +0100

    remove unused workflow

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): { status: boolean } {
-    return { status: true };
+    return { status: false };
   }
 }
