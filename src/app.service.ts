e7c387f9ed9d98d7cc5d5f5199a3b5f79cf21674
Christian Llansola <christian.llansola@mail.schwarz>
2024-01-27 16:25:25 +0100

    typeorm recovered

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { configApp } from './config/config.app';
 
 @Injectable()
 export class AppService {
   getHello(): { status: boolean } {
+    console.log(configApp().postgres.host);
     return { status: false };
   }
 }
